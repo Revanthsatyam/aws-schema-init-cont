57fd720a1d73da1586d8a488ec46469e24f1d1cc
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-22 20:14:15 +0530

    changes

--- run.sh
+++ run.sh
@@ -13,3 +13,11 @@ if [ "${schema_type}" == "DOCDB" ]; then
 
   mongo --ssl --host $mongo_host:27017 --sslCAFile /rds-combined-ca-bundle.pem --username $user_name --password $password <$component/schema/$component.js
 fi
+
+if [ "${schema_type}" == "MYSQL" ]; then
+  mysql_host=$(aws_ssm rds.$env.endpoint)
+  user_name=$(aws_ssm rds.$env.master_username)
+  password=$(aws_ssm rds.$env.master_password)
+
+  mysql -h $mysql_host -u$user_name -p$password <$component/schema/$component.sql
+fi
